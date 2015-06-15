a301dc917abc2f8656bc6e357abfa9a15eb00db6
Phuc Nguyen <phucnguyenpr@gmail.com>
2015-06-16 00:50:35 +0700

    Initial Commit

---
+++ SmartWorkspace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartWorkspace
+//
+//  Created by Phuc Nguyen on 6/16/15.
+//  Copyright (c) 2015 PHUCNGUYEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartWorkspace/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartWorkspace
+//
+//  Created by Phuc Nguyen on 6/16/15.
+//  Copyright (c) 2015 PHUCNGUYEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
